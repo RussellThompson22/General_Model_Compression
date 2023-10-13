25d48dc3314d339662509046fce7f81ee172c909
RussellThompson22 <132488976+RussellThompson22@users.noreply.github.com>
2023-10-12 20:38:57 -0400

    Update train.py

--- train.py
+++ train.py
@@ -2,7 +2,8 @@ import numpy as np
 import tensorflow_model_optimization as tfmot
 import wandb
 from tqdm.auto import tqdm
-from General_Model_Compression.data_tools import get_dataset_info
+from General_Model_Compression.data_tools import get_dataset_info, setup_data
+from General_Model_Compression import *
 
 def standardize_model_output(output):
     try:
